74db128fb7974cb7a1aa8960f905fe60428e3daa
Ramon Wenger <donramonloco+github@gmail.com>
2015-03-19 14:34:13 +0100

    removed the test functions again

--- js/controllers/main.controller.js
+++ js/controllers/main.controller.js
@@ -20,47 +20,5 @@
             $http.get('http://date.jsontest.com/');
         }
 
-        vm.waitForResponse = function () {
-            watchSomethingElseFor6Seconds().then(
-                function () {//success
-                    console.log('Yay!');
-                },
-                function () {//error
-                    console.log('Nay...');
-                });
-        }
-
-        function watchSomethingElseFor6Seconds() {
-            var deferred = $q.defer();
-            var timeout = false;
-
-            /*$scope.$watch('somethingElse', function (newVal, oldVal) {
-                console.log('running $watch');
-                if(newVal!=oldVal){
-                    console.log('new', newVal, 'old', oldVal);
-                    deferred.resolve(newVal);
-                }
-            });*/
-
-            $timeout(checkSomething, 50);
-
-            $timeout(function () {
-                //deferred.reject('Timeout reached.');
-                timeout = true;
-            }, 6000);
-
-            return deferred.promise;
-
-            function checkSomething(){
-                if(timeout){
-                   return false;
-                }
-                if(something>0){
-                    deferred.resolve();
-                    return true;
-                }
-                $timeout(checkSomething, 50);
-            }
-        }
     }
 })();
