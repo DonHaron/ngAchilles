6db108af3480a4e8f433006af6720f44e219646f
Ramon Wenger <donramonloco+github@gmail.com>
2015-03-26 16:04:09 +0100

    added some restful api goodness

--- js/controllers/main.controller.js
+++ js/controllers/main.controller.js
@@ -14,7 +14,6 @@
             {
                 id: 1,
                 name: 'Diagnose',
-
             },
             {
                 id: 2,
@@ -26,136 +25,175 @@
             },
         ];
 
-        for(var i=0;i<vm.entryTypes.length;i++){
-            vm.entryTypes[i].toString = function(){ return this.id; }
+        /*
+         * this is ugly, but need to do this with an open issue in the angular-select2 lib
+         * https://github.com/rubenv/angular-select2/issues/15
+         * TODO: change once the issue is resolved
+         * */
+        for (var i = 0; i < vm.entryTypes.length; i++) {
+            vm.entryTypes[i].toString = function () {
+                return this.id;
+            }
         }
 
-        vm.addAttribute = function(entry){
-            entry.attributes.push({
+        vm.addAttribute = function (treatment) {
+            treatment.entries.push({
                 content: '',
                 new: true
             });
         }
 
         vm.loadEntries = function () {
-            vm.entries = [
-                {
-                    date: '2015-03-19 14:21',
-                    user: 'bha',
-                    case: '???',
-                    attributes: [
-                        {
-                            type: {
-                                id: 123,
-                                name: 'Diagnose'
-                            }, //id of the type
-                            columns: [
-                                {
-                                    width: 10,
-                                    content: 'Hypochonder',
-                                    readonly: false,
-                                    wysiwyg: false,
-                                    validation: 'numeric'
-                                },
-                                {
-                                    width: 2,
-                                    content: 'asdf',
-                                    readonly: true,
-                                    wysiwyg: false,
-                                    validation: 'numeric'
-                                }
-                            ]
-                        },
-                        {
-                            name: 'Objektiv',
-                            content: '',
-                            columns: [
-                                {
-                                    width: 2,
-                                    content: '2',
-                                    readonly: false,
-                                    wysiwyg: false,
-                                    validation: 'numeric'
-                                },
-                                {
-                                    width: 4,
-                                    content: '4',
-                                    readonly: false,
-                                    wysiwyg: false,
-                                    validation: 'numeric'
-                                },
-                                {
-                                    width: 6,
-                                    content: '6',
-                                    readonly: false,
-                                    wysiwyg: false,
-                                    validation: 'numeric'
-                                }
-                            ]
-                        },
-                        {
-                            name: 'Lokalbefund',
-                            content: ''
-                        },
-                        {
-                            name: 'Beurteilung/Diag',
-                            content: ''
-                        },
-                        {
-                            name: 'Procedere',
-                            content: ''
-                        }
-                    ]
+            $http.get('http://192.168.1.145:37115/patient/70220/treatmentlist')
+                .then(function (response) {
+                    vm.treatments = response.data;
+                });
+//            vm.entries = [
+//                {
+//                    date: '2015-03-19 14:21',
+//                    user: 'bha',
+//                    case: '???',
+//                    attributes: [
+//                        {
+//                            type: {
+//                                id: 123,
+//                                name: 'Diagnose'
+//                            }, //id of the type
+//                            columns: [
+//                                {
+//                                    width: 10,
+//                                    content: 'Hypochonder',
+//                                    readonly: false,
+//                                    wysiwyg: false,
+//                                    validation: 'numeric'
+//                                },
+//                                {
+//                                    width: 2,
+//                                    content: 'asdf',
+//                                    readonly: true,
+//                                    wysiwyg: false,
+//                                    validation: 'numeric'
+//                                }
+//                            ]
+//                        },
+//                        {
+//                            name: 'Objektiv',
+//                            content: '',
+//                            columns: [
+//                                {
+//                                    width: 2,
+//                                    content: '2',
+//                                    readonly: false,
+//                                    wysiwyg: false,
+//                                    validation: 'numeric'
+//                                },
+//                                {
+//                                    width: 4,
+//                                    content: '4',
+//                                    readonly: false,
+//                                    wysiwyg: false,
+//                                    validation: 'numeric'
+//                                },
+//                                {
+//                                    width: 6,
+//                                    content: '6',
+//                                    readonly: false,
+//                                    wysiwyg: false,
+//                                    validation: 'numeric'
+//                                }
+//                            ]
+//                        },
+//                        {
+//                            name: 'Lokalbefund',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Beurteilung/Diag',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Procedere',
+//                            content: ''
+//                        }
+//                    ]
+//                },
+//                {
+//                    date: '2015-03-19 14:20',
+//                    user: 'bha',
+//                    case: '???',
+//                    type: 'success',
+//                    attributes: [
+//                        {
+//                            name: 'Subjektiv',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Objektiv',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Beurteilung/Diag',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Procedere',
+//                            content: ''
+//                        }
+//                    ]
+//                }
+//                ,
+//                {
+//                    date: '2013-08-06 09:06',
+//                    user: 'afr',
+//                    case: 'Krankheit',
+//                    attributes: [
+//                        {
+//                            name: 'Subjektiv',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Objektiv',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Beurteilung/Diag',
+//                            content: ''
+//                        },
+//                        {
+//                            name: 'Procedere',
+//                            content: ''
+//                        }
+//                    ]
+//                }
+//            ];
+        }
+
+        var count = 0;
+
+        vm.testPost = function () {
+            count++;
+
+            var treatmentEntry = {
+                "id": 31620,
+                "type": {
+                    "id": 6,
+                    "name": "Therapie Edited :"+count
                 },
-                {
-                    date: '2015-03-19 14:20',
-                    user: 'bha',
-                    case: '???',
-                    type: 'success',
-                    attributes: [
-                        {
-                            name: 'Subjektiv',
-                            content: ''
-                        },
-                        {
-                            name: 'Objektiv',
-                            content: ''
-                        },
-                        {
-                            name: 'Beurteilung/Diag',
-                            content: ''
-                        },
-                        {
-                            name: 'Procedere',
-                            content: ''
-                        }
-                    ]
-                }
-                ,
-                {
-                    date: '2013-08-06 09:06',
-                    user: 'afr',
-                    case: 'Krankheit',
-                    attributes: [
-                        {
-                            name: 'Subjektiv',
-                            content: ''
-                        },
-                        {
-                            name: 'Objektiv',
-                            content: ''
-                        },
-                        {
-                            name: 'Beurteilung/Diag',
-                            content: ''
-                        },
-                        {
-                            name: 'Procedere',
-                            content: ''
-                        }
-                    ]
-                }
-            ];
+                "columns": [
+                    {
+                        "content": "",
+                        "width": 12,
+                        "readonly": false,
+                        "wysiwyg": false,
+                        "validation": "none"
+                    }
+                ]
+            };
+
+            $http.put('http://192.168.1.145:37115/treatmententry/31620', treatmentEntry)
+                .then(function(response){
+                    console.log(response);
+                });
         }
 
         vm.loadEntries();
--- js/directives/treatment.directives.js
+++ js/directives/treatment.directives.js
@@ -6,13 +6,16 @@
         .directive('treatmentColumn', treatmentColumn)
         .directive('treatmentType', treatmentType);
 
-    function treatmentColumn(){
+    treatmentColumn.$inject = ['$http'];
+
+    function treatmentColumn($http){
         var directive = {
             restrict: 'E',
             scope: {
                 width: '@',
                 content: '=',
-                readonly: '@'
+                readonly: '@',
+                parent: '='
             },
             template: '<div ng-class="columnClass"><div class="form-group"><input class="form-control" ng-model="content" ng-disabled="{{readonly}}"></div></div>',
             link: link
@@ -22,10 +25,16 @@
 
         function link(scope, element, attrs){
             scope.columnClass = 'col-xs-'+attrs.width;
+
+            element.find('input').on('blur', function(e){
+                console.log('blurred');
+                $http.put('http://192.168.1.145:37115/treatmententry/'+scope.parent.id, scope.parent);
+            });
         }
     }
 
     treatmentType.$inject = ['$timeout'];
+
     function treatmentType($timeout){
         var directive = {
             restrict: 'E',
@@ -34,14 +43,14 @@
                 entry: '=',
                 types: '='
             },
-            template: '<div class="form-group"><span ng-show="entry.type">{{entry.type.name}}:</span><select2 class="form-control" ng-hide="entry.type" ng-model="entry.type" ng-options="type as type.name for type in types track by type.id"></select2></div>'
+            template: '<div class="form-group"><span ng-show="entry.type">{{entry.type.name}}</span><select2 class="form-control" ng-hide="entry.type" ng-model="entry.type" ng-options="type as type.name for type in types track by type.id"></select2></div>'
         };
 
         return directive;
 
         function link(scope, element, attrs){
             $timeout(function(){
-                if(!scope.entry.type && scope.entry.new){
+                if(scope.entry && !scope.entry.type && scope.entry.new){
                     element.find('input').select2('open');
                 }
             },50);
