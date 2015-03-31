787f516c6390aaef7bc8a8eee5160b551150459c
Ramon Wenger <donramonloco+github@gmail.com>
2015-03-31 16:21:32 +0200

    finally found & fixed the bug with the disappearing columns in the entries!

--- js/directives/treatment.directives.js
+++ js/directives/treatment.directives.js
@@ -3,9 +3,22 @@
 
     angular
         .module('achilles')
+        //.directive('treatment', treatment)
         .directive('treatmentEntry', treatmentEntry)
         .directive('treatmentColumn', treatmentColumn)
         .directive('treatmentType', treatmentType);
+
+//    function treatment(){
+//        var directive = {
+//            scope:{
+//                treatment: '='
+//            },
+//            restrict: 'E',
+//            template: ''
+//        };
+//
+//        return directive;
+//    }
 
     function treatmentEntry() {
         var directive = {
@@ -13,8 +26,8 @@
                 entry: '=',
                 types: '='
             },
+            restrict: 'E',
             template: '<li class="list-group-item">' +
-                '<pre>{{entry|json}}</pre>' +
                 '<div class="row">' +
                 '<div class="col-xs-4 col-sm-3 col-md-2">' +
                 '<treatment-type entry="entry" types="types"></treatment-type>' +
@@ -37,7 +50,11 @@
             var dc = this;
 
             dc.setEntry = function (entry) {
-                $scope.entry = entry;
+                //cannot just set the entry to be the new entry, otherwise we lose the association to the parent element
+                $scope.entry.treatmentId = entry.treatmentId;
+                $scope.entry.id = entry.id;
+                $scope.entry.type = entry.type;
+                $scope.entry.columns = entry.columns;
                 $scope.entryform.$setPristine();
             }
         }
@@ -75,29 +92,40 @@
         }
     }
 
-    treatmentType.$inject = ['$timeout'];
+    treatmentType.$inject = ['$timeout', '$http', 'urls'];
 
-    function treatmentType($timeout) {
+    function treatmentType($timeout, $http, urls) {
         var directive = {
             restrict: 'E',
             link: link,
             controller: controller,
             controllerAs: 'dm',
+            require: '^treatmentEntry',
             scope: {
                 entry: '=',
                 types: '='
             },
-            template: '<div class="form-group"><span ng-show="entry.type">{{entry.type.name}}</span><select2 class="form-control" ng-change="dm.store(entry)" ng-hide="entry.type" ng-model="entry.type" ng-options="type as type.name for type in types track by type.id"></select2></div>'
+            template: '<div class="form-group"><span ng-show="entry.type">{{entry.type.name}}</span><select2 class="form-control" ng-change="store(entry)" ng-hide="entry.type" ng-model="entry.type" ng-options="type as type.name for type in types track by type.id"></select2></div>'
         };
 
         return directive;
 
-        function link(scope, element, attrs) {
+        function link(scope, element, attrs, entryCtrl) {
             $timeout(function () {
                 if (scope.entry && !scope.entry.type && !scope.entry.id) {
                     element.find('input').select2('open');
                 }
             }, 50);
+
+            scope.store = function(entry){
+                $http.post(urls.treatmentEntry(), entry)
+                    .then(function (response) {
+                        entryCtrl.setEntry(response.data);
+                    }, function (error) {
+                        console.error(error);
+                        entryCtrl.setEntry({});
+                    });
+            }
         }
 
         controller.$inject = ['$http', '$scope', 'urls'];
@@ -109,13 +137,7 @@
             dm.store = function (entry) {
                 //TODO: maybe replace the url?
                 //$http.post('http://192.168.1.145:37114/treatmententry/', entry)
-                $http.post(urls.treatmentEntry(), entry)
-                    .then(function (response) {
-                        $scope.entry = response.data;
-                    }, function (error) {
-                        console.error(error);
-                        $scope.entry = {};
-                    });
+
             }
         }
     }
