8dfb9c34748961bd280260cfbb3c09a7d2b81813
lukaszaugg <lukas.zaugg@axonlab.ch>
2015-07-23 07:27:34 +0200

    Inserted Medilist first draft

--- js/controllers/medication.controller.js
+++ js/controllers/medication.controller.js
@@ -13,7 +13,18 @@
         var vm = this;
 
         vm.medicationList = [
-            { name : 'Aspirin'}, { name : 'Viagra'}
+            { name : 'Aspirin', taking : "0-1-0-1", indication : "Stinken", date : "1.1.15 bis auf weiteres",
+              history : [ {name : 'Aspirin', taking : "0-0-0-1", indication : "Stinken", date : "1.1.14 bis auf weiteres"},
+                  {name : 'Aspirin', taking : "0-1-0-2", indication : "Stinken", date : "1.5.13 bis auf weiteres"},
+                  {name : 'Aspirin', taking : "0-1-0-4", indication : "Stinken", date : "1.1.11 bis auf weiteres"}
+              ]
+            },
+            { name : 'Viagra', taking : "0-1-0-1", indication : "Stinken", date : "1.1.15 bis auf weiteres",
+                history : [ {name : 'Viagra', taking : "1-1-0-1", indication : "Stinken", date : "6.9.14 bis auf weiteres"},
+                    {name : 'Viagra', taking : "0-2-0-1", indication : "Stinken", date : "3.1.14 bis auf weiteres"},
+                    {name : 'Viagra', taking : "0-1-0-4", indication : "Stinken", date : "1.1.12 bis auf weiteres"}
+                ]
+            }
         ];
 
     }
