178a6919cddec43820cd4cb0c281d61b4d060303
lukaszaugg <lukas.zaugg@axonlab.ch>
2015-07-31 11:37:07 +0200

    First try with 24 grid

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ var achillesConfig = {
     process: parseInt(getURLParameter("process")) || 1234,
     //patient: parseInt(getURLParameter("patient")) || 1369,
     patient: parseInt(getURLParameter("patient")) || 1,
-    port: 37113,
-    baseUrl: '192.168.1.137'
+    port: 36611,
+    baseUrl: '192.168.0.12'
 };
 function getURLParameter(name) {
     return (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20')||null
